713f4e8119c59a0970387eb721fab8d0830ad583
Ransom Ahanmisi <60149483+Ransom8@users.noreply.github.com>
2020-08-26 17:48:59 +0100

    First Commit

--- app/src/androidTest/java/com/example/week_6registration_form/SecondActivityTest.kt
+++ app/src/androidTest/java/com/example/week_6registration_form/SecondActivityTest.kt
@@ -3,15 +3,12 @@ package com.example.week_6registration_form
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.junit.Test
-
-import org.junit.Assert.*
 import org.junit.Rule
+import org.junit.Test
 
 class SecondActivityTest {
 
@@ -32,30 +29,11 @@ class SecondActivityTest {
     }
 
     /**
-     * Test navigation from SecondaryActivity to MainActivity
-     */
-    @Test
-    fun test_backPress_toMainActivity() {
-
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        Espresso.onView(withId(R.id.button)).perform(ViewActions.click())
-
-        Espresso.onView(withId(R.id.secondActivity))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-
-        Espresso.pressBack()
-
-        Espresso.onView(withId(R.id.mainActivity))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-    }
-
-    /**
      * This checks if all the views are visible
      */
     @Test
     fun test_allViews() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        val activityScenario = ActivityScenario.launch(SecondActivity::class.java)
         Espresso.onView(withId(R.id.imageView)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         Espresso.onView(withId(R.id.imageView3)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         Espresso.onView(withId(R.id.imageView4)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
